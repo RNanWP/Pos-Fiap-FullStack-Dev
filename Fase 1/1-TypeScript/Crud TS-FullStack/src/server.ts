99e83b49213b940216fe3a6fae7cab9f44d9475b
RNanWP <renan92011@hotmail.com>
2025-04-06 19:33:59 -0300

    feat: Criando conexão express

--- Fase 1/1-TypeScript/Crud TS-FullStack/src/server.ts
+++ Fase 1/1-TypeScript/Crud TS-FullStack/src/server.ts
@@ -1,1 +1,14 @@
-import fastify from "fastify";
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.get("/", (request, response) => {
+  response.send("hello world");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
