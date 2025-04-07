a70cc3a37cde6b382060ac824236a8e025e82341
RNanWP <renan92011@hotmail.com>
2025-04-07 18:46:06 -0300

    Feat: criando biblioteca.ts e js

---
+++ Fase-1/1-TypeScript/1-Básico/biblioteca.js
@@ -0,0 +1,13 @@
+var Livro = /** @class */ (function () {
+    function Livro(titulo, anoPublicacao) {
+        this.titulo = titulo;
+        this.anoPublicacao = anoPublicacao;
+    }
+    Livro.prototype.exibirDetalhes = function () {
+        console.log("Livro: ".concat(this.titulo, ", publicado em ").concat(this.anoPublicacao, "."));
+    };
+    return Livro;
+}());
+// Criando instância da classe Livro
+var livroFavorito = new Livro("O Nome do Vento", 2007);
+livroFavorito.exibirDetalhes();
---
+++ Fase-1/1-TypeScript/1-Básico/biblioteca.ts
@@ -0,0 +1,17 @@
+class Livro {
+   titulo: string;
+   anoPublicacao: number;
+
+   constructor(titulo: string, anoPublicacao: number) {
+       this.titulo = titulo;
+       this.anoPublicacao = anoPublicacao;
+   }
+
+   exibirDetalhes() {
+       console.log(`Livro: ${this.titulo}, publicado em ${this.anoPublicacao}.`);
+   }
+}
+
+// Criando instância da classe Livro
+const livroFavorito = new Livro("O Nome do Vento", 2007);
+livroFavorito.exibirDetalhes();
