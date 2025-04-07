950717dd28ec50d3a81ace6af9516edb7ec613fb
RNanWP <renan92011@hotmail.com>
2025-04-07 14:08:04 -0300

    Feat: criando arquivo dominios.java

---
+++ Fase-1/2-Domain-Driven-Design(DDD)/Introdução DDD/dominios.java
@@ -0,0 +1,10 @@
+class Marketing (
+   private int id_lead;
+   private string nome_lead;
+)
+
+class Admissoes (
+   private int id_prospect;
+)
+
+// Falando sobre a mesma coisa com usando formas diferentes de se expressar, "Subdomínios"
