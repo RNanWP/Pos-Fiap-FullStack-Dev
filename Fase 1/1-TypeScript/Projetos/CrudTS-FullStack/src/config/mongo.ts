557486c29bf3e916ebe66c8ac5bda47c22d8ea01
RNanWP <renan92011@hotmail.com>
2025-05-02 21:23:41 -0300

    Feat: criando mongo.ts e Livros.ts

---
+++ Fase 1/1-TypeScript/Projetos/CrudTS-FullStack/src/config/mongo.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("✅ Conectado ao MongoDB");
+  } catch (error) {
+    console.error("❌ Erro ao conectar no MongoDB:", error);
+    process.exit(1);
+  }
+};
---
+++ Fase 1/1-TypeScript/Projetos/CrudTS-FullStack/src/models/Livros.ts
@@ -0,0 +1,11 @@
+import { Schema, model } from "mongoose";
+
+const LivroSchema = new Schema({
+  titulo: { type: String, required: true },
+  autor: { type: String, required: true },
+  isbn: { type: String, required: true },
+  ano: { type: Number, required: true },
+  editora: { type: String },
+});
+
+export const Livro = model("Livro", LivroSchema);
